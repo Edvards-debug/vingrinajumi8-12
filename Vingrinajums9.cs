7866a533cf2c3137a2ffa7088b6acc8cde02a33a
Edvards-debug <70687857+Edvards-debug@users.noreply.github.com>
2021-10-20 16:05:14 +0300

    Create Vingrinajums9.cs

---
+++ Vingrinajums9.cs
@@ -0,0 +1,11 @@
+using System;
+public class Program
+{
+	public static void Main()
+	{
+    Console.WriteLine("Write how many steps you will take");
+      int steps = Convert.ToInt32(Console.ReadLine());
+      Array.Add(steps);
+      Console.WriteLine("you have made " + steps + "steps");
+    }
+}
