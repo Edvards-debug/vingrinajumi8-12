a5c0394b50fdbfe9b15684e2e1d4c28cad17f41c
Edvards-debug <70687857+Edvards-debug@users.noreply.github.com>
2021-11-04 14:15:51 +0200

    .
    .

---
+++ New folder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class GFG {
+
+	public static void Main()
+	{
+        int numberOne = 12932;
+        int numberTwo = -2828472;
+		double x = numberOne;
+        double y = numberTwo;
+
+        if (numberOne > numberTwo)
+            Console.WriteLine(numberTwo);
+        if (numberTwo > numberOne)
+            Console.WriteLine(numberOne);
+
+		Console.Write(Math.Sqrt(x));
+
+	}
+}
---
+++ Vingrinajums11.cs
@@ -0,0 +1,13 @@
+using System;
+
+class GFG {
+
+// Main method
+	public static void Main()
+	{
+		double x = -81;
+
+		// Input Negative value, Output square root of x
+		Console.Write(Math.Sqrt(x));
+	}
+}
