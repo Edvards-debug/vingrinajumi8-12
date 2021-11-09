81e35cc480a753af9c84e15b492c7b1d60361c9d
Edvards-debug <70687857+Edvards-debug@users.noreply.github.com>
2021-11-09 15:49:12 +0200

    Update Vingrinajums11.cs

--- Vingrinajums11.cs
+++ Vingrinajums11.cs
@@ -1,17 +1,17 @@
 using System;
 
-class GFG {
-
-	
-	public static void Main()
-	{
-		double x = 12932 ;
-
-	
-		Console.WriteLine(Math.Sqrt(x));
-		double y = -2828472;
-
-		
-		Console.Write(Math.Sqrt(y));
-	}
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int y = 12932;
+      int x = -2828472;
+      if (y > x)
+      {
+        Console.WriteLine("Mazākā vērtība - " + Math.Sqrt(y));
+      }
+    }
+  }
 }
